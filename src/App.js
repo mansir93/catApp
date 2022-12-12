987e257afe894f5e60b703103660aa3992ad5787
mansir93 <mansiraziz93@gmail.com>
2022-12-12 08:12:17 +0000

    reupload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Header from './components/Header';
+import Showcat from './components/Showcat';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+    <Header />
+
+    <Showcat />
+    </>
+  )
+}
+
+export default App;
